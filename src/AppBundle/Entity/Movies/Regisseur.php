dcff2762aaa66154daa3dcc168cdb130724426c7
PhantomPhr3ak <34191989+PhantomPhr3ak@users.noreply.github.com>
2018-02-28 16:20:35 +0100

    added table & entity regisseur

---
+++ src/AppBundle/Entity/Movies/Regisseur.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity\Movies;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Regisseur
+ *
+ * @ORM\Table(name="movies_regisseur")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\Movies\RegisseurRepository")
+ */
+class Regisseur
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fullName", type="string", length=255)
+     */
+    private $fullName;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthday", type="datetime")
+     */
+    private $birthday;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fullName
+     *
+     * @param string $fullName
+     *
+     * @return Regisseur
+     */
+    public function setFullName($fullName)
+    {
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+
+    /**
+     * Get fullName
+     *
+     * @return string
+     */
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+
+    /**
+     * Set birthday
+     *
+     * @param \DateTime $birthday
+     *
+     * @return Regisseur
+     */
+    public function setBirthday($birthday)
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+
+    /**
+     * Get birthday
+     *
+     * @return \DateTime
+     */
+    public function getBirthday()
+    {
+        return $this->birthday;
+    }
+}
+
